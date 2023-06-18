e31d846c72eb109ef5dbec970b6a65742a41135c
Tauhid Hasan <tauhidhasan11@gmail.com>
2023-06-19 00:19:52 +0300

    project setup and deployment setup completed for cow hut assignment

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+// cors
+app.use((0, cors_1.default)());
+// parser
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// Testing
+app.get('/', (req, res) => {
+    res.send('This is  a Backend for Online Cow Selling  for Eid Ul Adha...');
+});
+exports.default = app;
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+
+// cors
+app.use(cors())
+
+// parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Testing
+app.get('/', (req: Request, res: Response) => {
+  res.send('This is  a Backend for Online Cow Selling  for Eid Ul Adha...')
+})
+
+export default app

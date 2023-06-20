fba06d39e0a7b2eb15f226d75546156dec7562a4
Tauhid Hasan <tauhidhasan11@gmail.com>
2023-06-20 21:20:37 +0300

    winston error logger added and tested for single error

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import logger from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database connected successfully')
+    logger.info('Database connected successfully')
     app.listen(config.port, () => {
       console.log(`Application listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('Failed to connect to database', err)
+    logger.error('Failed to connect to database', err)
   }
 }
 

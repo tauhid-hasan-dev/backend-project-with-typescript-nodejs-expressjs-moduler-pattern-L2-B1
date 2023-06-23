8e1dc6b46462b85e98ebd6268ed244e3390ed188
Tauhid Hasan <tauhidhasan11@gmail.com>
2023-06-23 22:45:29 +0300

    testing custom error (ApiError) is done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+
 import express, { Application } from 'express'
 import cors from 'cors'
 import userRouter from './app/module/users/user.route'
 import globalErrorHandler from './middlewares/globalErrorHandler'
+/* import ApiError from './errors/ApiError' */
 
 const app: Application = express()
 
@@ -11,6 +13,10 @@ app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+/* app.use('/', ()=>{
+    throw new ApiError(400, 'ore baba re')
+}) */
 
 // application routes
 app.use('/api/v1', userRouter)
--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -2,10 +2,17 @@ import { ErrorRequestHandler, NextFunction } from 'express';
 import { IGenericErrorMessage } from '../interfaces/error';
 import config from '../config';
 import handleValidationError from '../errors/validationError';
+import ApiError from '../errors/ApiError';
+import { errorLogger } from '../shared/logger';
 
 const globalErrorHandler : ErrorRequestHandler = (
     error, req, res, next: NextFunction
   ) => {
+
+    // we are doing this because in the development we don not want see the logger(console is enough) but for the production we will see the logger
+    config.env === 'development'
+    ? console.log('🌍 globalErrorHandler -', error)
+    : errorLogger.error('🌍 globalErrorHandler -', error);
      
      let statusCode = 500;
      let message = 'Internal Server Error!';
@@ -16,7 +23,19 @@ const globalErrorHandler : ErrorRequestHandler = (
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
         errorMessages = simplifiedError.errorMessages;
-      } else if (error instanceof Error) {
+      } else if (error instanceof ApiError) {
+        (statusCode = error?.statusCode),
+          (message = error?.message),
+          (errorMessages = error?.message
+            ? [
+                {
+                  path: '',
+                  message: error?.message,
+                },
+              ]
+            : []);
+      }
+      else if (error instanceof Error) {
         message = error?.message;
         errorMessages = error.message
           ? [
@@ -28,6 +47,7 @@ const globalErrorHandler : ErrorRequestHandler = (
           : [];
       }
 
+    // we will send errors to the frontend in this pattern (this will be common pattern for all the errors)
      res.status(statusCode).json({
         success: false,
         message,

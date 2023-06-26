b5f1460a2c0ca433e6eda113a9b7686c0f019d43
Tauhid Hasan <tauhidhasan11@gmail.com>
2023-06-27 01:46:59 +0300

    cast error handle successfully

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -7,6 +7,7 @@ import ApiError from '../errors/ApiError';
 import { errorLogger } from '../shared/logger';
 import { ZodError } from 'zod';
 import handleZodError from '../errors/handleZodError';
+import handleCastError from '../errors/handleCastError';
 
 const globalErrorHandler : ErrorRequestHandler = (
     error, req, res, next
@@ -25,7 +26,13 @@ const globalErrorHandler : ErrorRequestHandler = (
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
         errorMessages = simplifiedError.errorMessages;
-      } else if(error instanceof ZodError){
+      } 
+      else if (error?.name === 'CastError') {
+        const simplifiedError = handleCastError(error);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorMessages = simplifiedError.errorMessages}
+      else if(error instanceof ZodError){
         const simplifiedError = handleZodError(error);
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
